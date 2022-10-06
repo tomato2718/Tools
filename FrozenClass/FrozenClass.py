31293459555f2234d91e2bfb3ef88a6b849c7539
tomato2718 <qweasdzxc030@gmail.com>
2022-10-06 15:25:12 +0800

    10.06.22

--- FrozenClass/FrozenClass.py
+++ FrozenClass/FrozenClass.py
@@ -13,21 +13,3 @@ def FrozenClass(target):
     target.__init__ = __init__
     target.__setattr__ = __setattr__
     return target 
-
-
-def main():
-    try:
-        myExample = example('foo')
-        myExample.hello = 'hi'
-    except Exception as e:
-        print(e)
-        print(myExample.__dict__)
-
-@FrozenClass
-class example():
-    def __init__(self, foo):
-        self.foo = foo
-        self.bar = 'bar'
-
-if __name__ == '__main__':
-    main()
