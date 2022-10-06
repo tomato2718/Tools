56fdeff0ebb05d07704fa2bf7321178d00cc6a8c
tomato2718 <qweasdzxc030@gmail.com>
2022-10-06 17:20:35 +0800

    10.06.22

--- QuickLogging/quickLogging.py
+++ QuickLogging/quickLogging.py
@@ -4,22 +4,14 @@ from functools import wraps
 
 
 class QuickLogging():
-    def __init__(self, path = "./Configs/logging_config.yml", getLoggers = ['main']):
+    def __init__(self, path = "./Configs/logging_config.yml"):
         self.path = os.path.abspath(path)
-        self.getLoggers = getLoggers
     def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 self.__read_config()
                 logging.config.dictConfig(self.config)
-                logger_dict = {}
-                for _logger in self.getLoggers:
-                    if _logger not in self.config['loggers']:
-                        raise Exception('no logger %s found!'%_logger)
-                    logger_dict[_logger] = logging.getLogger(_logger)
-
-                kwargs['quickLogger'] = logger_dict
             except Exception as e:
                 logging.error(e)
                 os._exit(0)
