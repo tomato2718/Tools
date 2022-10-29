11601296520eabe1bec76791894173fdccc42961
tomato2718 <qweasdzxc030@gmail.com>
2022-10-29 12:58:33 +0800

    up

--- argparser/README.md
+++ argparser/README.md
@@ -1,7 +1,14 @@
+## ArgParser
 will update later
 
+## Usage
 ```py
 test_case: str = None
+test_cases: list = None
 show_args: bool = False
 parser_id: str = None
 ```
+
+## Known bugs
+testcase(s) do not support space ` ` now, use `\n` instead
+e.g. `hello,\nworld!` 
--- argparser/argparser/_parsers.py
+++ argparser/argparser/_parsers.py
@@ -1,13 +1,54 @@
 import argparse
+import re
 
 # for single parser
 # default
 class GetParser():
     @staticmethod
-    def get_parser(_):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--test', '-t', help='test' )
-        parser.add_argument('--suffix', '-s', help='suffix')
+    def get_parser(_) -> argparse.ArgumentParser:
+        # instance parser
+        parser = argparse.ArgumentParser(prog = 'My Program',
+                                         description = 'Description of the program',
+                                         epilog = 'Text at the buttom')
+    
+        # show the version of program
+        parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+        
+        # store arguments with no flag
+        # support int, ?, *, +
+        parser.add_argument('first',
+                            nargs='?', 
+                            help='store first argument with no flag')
+
+        parser.add_argument('args',
+                            nargs='*', 
+                            help='store everything else with no flag into a list')
+        
+        # store arguments with flag
+        parser.add_argument('--something-important', '-i',
+                            dest='my_requirement', required=True,
+                            metavar='<requirement>', help='store something required')
+
+        parser.add_argument('--store-string', '-s',
+                            dest='my_string', type=str, default='String',
+                            metavar='<string>', help='store string')
+
+        parser.add_argument('--store-value', '-a',
+                            dest='my_value', type=int, default=12345,
+                            metavar='<value>', help='store value')
+
+        parser.add_argument('--store-const', '-c',
+                            dest='my_constant', action='store_const', const='my_const',
+                            metavar=None, help='store constant' )
+
+        parser.add_argument('--store-true', '-t',
+                            dest='my_true_value', action='store_true', default=False,
+                            help='store true')
+
+        parser.add_argument('--store-false', '-f',
+                            dest='my_false_value', action='store_false', default=True,
+                            help='store false')
+        
         return parser 
 
 # for multiple parser
@@ -25,13 +66,11 @@ class GetParsers():
 
     @staticmethod
     def __main_parser() -> argparse.ArgumentParser:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--test', '-t', help='test' )
-        parser.add_argument('--suffix', '-s', help='suffix')
+        parser = None
         return parser
 
     @staticmethod
     def __sub_parser() -> argparse.ArgumentParser:
-        parser = argparse.ArgumentParser()
+        parser = None
         
         return parser
