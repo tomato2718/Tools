62465d13955e00dcb54b215d1b4d3da631c06eba
tomato2718 <qweasdzxc030@gmail.com>
2022-10-31 12:13:08 +0800

    ~

--- argparser/argparser/_parsers.py
+++ argparser/argparser/_parsers.py
@@ -7,50 +7,60 @@ from ._utils import *
 class GetParser():
     @staticmethod
     def get_parser(_) -> argparse.ArgumentParser:
-        # instance parser
         parser = argparse.ArgumentParser(prog='My Program',
-                                         description='Description of the program',
-                                         epilog='Text at the buttom')
-    
-        # show the version of program
-        parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+                                        description='Description of the program',
+                                        epilog='Text at the buttom',
+                                        add_help=False)
+        # program informations
+        information = parser.add_argument_group('Program Informations')
+        information.add_argument('--help', '-h',
+                                action='help',
+                                help= 'show this help message and exit')
         
-        # store arguments with no flag
-        # support int, ?, *, +
-        parser.add_argument('first',
-                            nargs='?', 
-                            help='store first argument with no flag')
-
-        parser.add_argument('args',
-                            nargs='*', 
-                            help='store everything else with no flag into a list')
+        information.add_argument('--version', '-v',
+                            action='version',
+                            version='%(prog)s 1.0')
         
-        # store arguments with flag
-        parser.add_argument('--something-important', '-i',
-                            dest='my_requirement', required=True,
-                            metavar='<requirement>', help='store something required')
-
-        parser.add_argument('--store-string', '-s',
+        # store required arguments
+        required = parser.add_argument_group('Required Arguments')
+        required.add_argument('--store-required', '-r',
+                            dest='my_required', type=str,
+                            metavar='<string>', help='store required')
+        
+        # store optional arguments
+        optional = parser.add_argument_group('Optional Arguments')
+        optional.add_argument('--store-string', '-s',
                             dest='my_string', type=str, default='String',
                             metavar='<string>', help='store string')
 
-        parser.add_argument('--store-value', '-a',
+        optional.add_argument('--store-value', '-a',
                             dest='my_value', type=int, default=12345,
                             metavar='<value>', help='store value')
 
-        parser.add_argument('--store-const', '-c',
+        optional.add_argument('--store-const', '-c',
                             dest='my_constant', action='store_const', const='my_const',
                             metavar=None, help='store constant' )
 
-        parser.add_argument('--store-true', '-t',
+        optional.add_argument('--store-true', '-t',
                             dest='my_true_value', action='store_true', default=False,
                             help='store true')
 
-        parser.add_argument('--store-false', '-f',
+        optional.add_argument('--store-false', '-f',
                             dest='my_false_value', action='store_false', default=True,
                             help='store false')
-        
-        return parser 
+
+        # store arguments without flag
+        other = parser.add_argument_group('Other Input')
+        other.add_argument('first',
+                            nargs='?', 
+                            help='store first argument with no flag')
+
+        other.add_argument('args',
+                            nargs='*', 
+                            help='store everything else with no flag into a list')
+            
+            
+        return parser
 
 # for multiple parser
 # use it if your project have multiple entry
